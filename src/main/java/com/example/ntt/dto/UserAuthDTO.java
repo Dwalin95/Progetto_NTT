0d3e79a2808dc3cb85ff64f13030150738cd0c2c
Doctorwhoita <73638711+Doctorwhoita@users.noreply.github.com>
2023-03-24 10:49:06 +0100

    Update UserAuthDTO.java

--- src/main/java/com/example/ntt/dto/UserAuthDTO.java
+++ src/main/java/com/example/ntt/dto/UserAuthDTO.java
@@ -1,16 +1,10 @@
 package com.example.ntt.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Value
 public class UserAuthDTO {
-    private String email;
-    private String password;
+    String email;
+    String password;
 
 }
